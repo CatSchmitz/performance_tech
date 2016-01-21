04e4c80ef02bf7026da683c9f5c247180a25ce88
Jonathan Beilin <jbeilin@gmail.com>
2016-01-20 19:20:40 -0500

    rough commit before sync with cat

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'oMneWsagJShov1Eywm9UQ9OjU',
+  consumer_secret:      'uJmuGePPbkxFxcqSV02IqmXd6jjqAc3siaxiRXoivljnOt6Jsf',
+  access_token:         '16136050-e9pEkJJPwW3IJLvVjvxYZTEhYuFZswZYTbxKCqaEs',
+  access_token_secret:  'd3Maawmu3aHzaqPuDJeN2ZXjArPd2YSSZUmSm3PiDyW0o'
+};
